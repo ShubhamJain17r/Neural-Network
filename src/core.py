273c2d1a27f753a03e4bbcdc588b2aad8f7afbab
ShubhamJain17r <shubhamjain00050@gmail.com>
2026-01-14 15:35:09 +0530

    core completed

--- src/core.py
+++ src/core.py
@@ -3,21 +3,27 @@ from abc import ABC, abstractmethod
 
 class Tensor:
     def __init__(self, data, requires_grad = False, name = None):
-        pass
+        self.data = np.asarray(data)
+        self.grad = np.zeros_like(self.data)
+        self.requires_grad = requires_grad
+        self.name = name
     def zero_grad(self):
-        pass
+        if self.requires_grad:
+            self.grad = np.zeros_like(self.data)
 
 class Parameter(Tensor):
     def __init__(self, data, requires_grad = True, name = None):
         super().__init__(data, requires_grad, name)
-        pass
     
 class Module(ABC):
     def __init__(self):
-        pass
+        object.__setattr__(self, "_parameters", {})
+        object.__setattr__(self, "_modules", {})
 
     def __setattr__(self, name, value):
-        pass
+        if isinstance(value, Module):
+            self._modules[name] = value
+        object.__setattr__(self, name, value)
     
     @abstractmethod
     def forward(self, *inputs):
@@ -28,7 +34,11 @@ class Module(ABC):
         raise NotImplementedError
     
     def parameters(self):
-        pass
+        for p in self._parameters.values():
+            yield p
+        for m in self._modules.values():
+            yield from m.parameters()
 
     def zero_grad(self):
-        pass
+        for p in self.parameters():
+            p.zero_grad()
